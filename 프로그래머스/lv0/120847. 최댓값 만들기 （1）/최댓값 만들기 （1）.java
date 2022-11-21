aabe97c59173657b31c6d63603d971c46849355d
seinHan <104081143+seinni2@users.noreply.github.com>
2022-11-21 17:12:56 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.33 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,18 +1,14 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] numbers) {
         
        int max = 0;
         
-       for(int i=0; i<numbers.length; i++){ 
-           for(int j=1; j<numbers.length-1; j++){
-               
-               if(max<numbers[i]*numbers[j]){
-                    max = numbers[i]*numbers[j];
-                   
-               }
-              
-           }
-       }
-        return max;
+       Arrays.sort(numbers);
+       
+       max = numbers[numbers.length-1] * numbers[numbers.length-2];
+        
+       return max;
     }
 }
