3a0b5c79787cea8747dc9e5e4f4601e048a2c609
seinHan <104081143+seinni2@users.noreply.github.com>
2022-11-21 20:53:25 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.01 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.java
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -3,10 +3,7 @@ class Solution {
         int[] result = new int[strlist.length];
        
         for(int i=0; i<strlist.length; i++){
-            String str = strlist[i];
-            String[] str_arr= str.split("");
-            
-            result[i] = str_arr.length;
+            result[i] = strlist[i].length();
         }
         return result;
     }
