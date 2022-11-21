965994ef32a66f731d6864370de75b7daa5b2620
seinHan <104081143+seinni2@users.noreply.github.com>
2022-11-21 15:00:19 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.01 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] array, int height) {
+        
+        int count = 0;
+        
+        for(int i=0; i<array.length; i++){
+            if(height < array[i]){
+                count += 1;
+            }
+        }
+        
+        return count;
+    }
+}
